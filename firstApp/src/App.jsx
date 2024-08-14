424dac894482749c731462d8f8aaa7101de51e14
unawarexi <unawarexi@gmail.com>
2024-08-14 11:27:14 +0100

    StateManagement and conditional Rendering and Routing

--- firstApp/src/App.jsx
+++ firstApp/src/App.jsx
@@ -1,23 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AboutPage from "./pages/AboutPage";
 import HeroSection from "./components/HeroSection";
 import MainLayout from "./layouts/MainLayout";
+import SecondaryHeroSection from "./components/SecondaryHeroSection";
 
 function App() {
+  const [isChange, setIsChange] = useState(false);
+
+  // Toggle the component being rendered
+  const toggleElements = () => {
+    setIsChange((prevState) => !prevState);
+  };
+
   return (
     <div>
-      <>
-        <BrowserRouter>
-          <MainLayout>
-            <Routes>
-              <Route path="/" element={<HeroSection />} />
-              <Route path="/about" element={<AboutPage />} />
-            </Routes>
-          </MainLayout>
-        </BrowserRouter>
-      </>
+      <button
+        onClick={toggleElements}
+        className="py-2 px-4 bg-blue-500 text-white font-semibold"
+      >
+        {" "}
+        CLICK TO CHANGE
+      </button>
+      <BrowserRouter>
+        <MainLayout>
+          <Routes>
+            <Route
+              path="/"
+              element={isChange ? <HeroSection /> : <SecondaryHeroSection />}
+            />
+            <Route path="/about" element={<AboutPage />} />
+          </Routes>
+        </MainLayout>
+      </BrowserRouter>
     </div>
   );
 }
--- firstApp/src/components/HeroSection.jsx
+++ firstApp/src/components/HeroSection.jsx
@@ -2,13 +2,17 @@ import React from "react";
 import Images from "../assets/Image";
 
 const HeroSection = () => {
+  // useState and create toggleButton function
+
   return (
     <section className="text-gray-600 body-font">
       <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+          {/* ----------- USE isChange for logic and instructions ------------- */}
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
             Knausgaard typewriter readymade marfa
           </h1>
+
           <p className="mb-8 leading-relaxed">
             Chillwave portland ugh, knausgaard fam polaroid iPhone. Man braid
             swag typewriter affogato, hella selvage wolf narwhal dreamcatcher.
@@ -28,6 +32,8 @@ const HeroSection = () => {
                 className="w-full bg-gray-100 rounded border bg-opacity-50 border-gray-300 focus:ring-2 focus:ring-indigo-200 focus:bg-transparent focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
               />
             </div>
+
+            {/* ----------- onclick function call ------------- */}
             <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
               Button
             </button>
